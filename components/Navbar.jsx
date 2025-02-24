a06a26fbbce09772267a05b38e7df017dca3ed4f
aihridoy <cse_1912020168@lus.ac.bd>
2025-02-25 03:39:09 +0600

    feat: google font added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,28 +1,29 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
     return (
         <nav>
     <div class="container flex justify-between py-6">
-      <a href="index.html">
+      <Link href="/">
         <img src="/assets/images/logo.png" alt="" class="object-cover h-[40px]" />
-      </a>
+      </Link>
 
       <ul class="flex gap-4 text-sm text-gray-500">
         <li class="py-2 active">
-          <a href="./index.html">Home</a>
+          <Link href="/">Home</Link>
         </li>
 
         <li class="py-2">
-          <a href="./index.html">Recipe</a>
+          <Link href="/categorized">Recipe</Link>
         </li>
 
         <li class="py-2">
-          <a href="./index.html">About us</a>
+          <Link href="/">About us</Link>
         </li>
 
         <li class="py-2 bg-[#eb4a36] px-6 rounded-md text-white content-center">
-          <a href="./login.html">Login</a>
+          <Link href="/login">Login</Link>
         </li>
       </ul>
     </div>
